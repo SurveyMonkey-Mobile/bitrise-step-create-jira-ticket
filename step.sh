62fd0cdec4333dcda89b3888a9671e5fc83d1191
Patrick Mullen <patrickm@surveymonkey.com>
2023-05-05 15:04:32 -0700

    Change step.sh to match jira api for creating tickets, add required variables to step.yml

--- step.sh
+++ step.sh
@@ -5,51 +5,37 @@ green=$'\e[32m'
 reset=$'\e[0m'
 
 body='{
-  "description": "'${version_description}'",
-  "name": "'${version_name}'",
-  "archived": false,
-  "project": "'${project_prefix}'",
+  "fields": {
+    "description": "'${ticket_description}'",
+    "name": "'${ticket_name}'",
+    "project": "'${ticket_project}'"
+  }
 '
 
 today=$(date +'%Y-%m-%d')
 
-if [ "$version_released" == "yes" ]; 
-then
-body+='  "released": true,
-  "releaseDate": "'$today'",
-'
-fi
-body+='  "startDate": "'$today'"
-}'
+res="$(curl -u $jira_user:$jira_token -X POST -H 'Content-Type: application/json' --data-raw "${body}" https://${jira_domain}/rest/api/3/issue)"
 
+echo $res
 
-res="$(curl -u $jira_user:$jira_token -X POST -H 'Content-Type: application/json' --data-raw "${body}" https://${jira_domain}/rest/api/2/version)"
-
-JIRA_VERSION_URL=""
+JIRA_TICKET_URL=""
 
 if [[ $res == *"errorMessages"* ]]; then
   error="$(echo $res | jq '.errors .name' | tr -d '"')"
   echo $'\t'"${red}❗️ Failed $error ${reset}"
 else
-  name="$(echo $res | jq '.name' | tr -d '"')"
   id="$(echo $res | jq '.id' | tr -d '"')"
-  description="$(echo $res | jq '.description' | tr -d '"')"
-  projectId="$(echo $res | jq '.projectId' | tr -d '"')"
-  project="$(echo $res | jq '.project' | tr -d '"')"
+  key="$(echo $res | jq '.key' | tr -d '"')"
   self="$(echo $res | jq '.self' | tr -d '"')"
   echo $'\t'"${green}✅ Success!${reset}"
   echo "Id = " $id
-  echo "Name = " $name
-  echo "Description = " $description
-  echo "Project ID = " $projectId
-  echo "Project = " $project
-  echo "API = " $self
+  echo "key = " $key
 
-  JIRA_VERSION_URL="https://${jira_domain}/projects/${project_prefix}/versions/${id}"
-  echo "JIRA VERSION = " $JIRA_VERSION_URL
+  JIRA_TICKET_URL="https://${jira_domain}/browse/${key}"
+  echo "JIRA TICKET = " $JIRA_TICKET_URL
 fi
 
-envman add --key JIRA_VERSION_URL --value "${JIRA_VERSION_URL}"
+envman add --key JIRA_TICKET_URL --value "${JIRA_TICKET_URL}"
 
 
 
